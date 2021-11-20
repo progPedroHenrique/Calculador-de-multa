3c5130815c381ed9d79a96aaf97a7eac33341bc3
pyPedroHenrique <94720134+pyPedroHenrique@users.noreply.github.com>
2021-11-19 23:19:28 -0300

    Add files via upload

---
+++ calculador.py
@@ -0,0 +1,18 @@
+#import
+
+from time import sleep
+
+#functions
+
+def calc(velocity):
+	return (velocity * 7) - 80
+
+#vars
+
+velocityUser = None
+
+#console
+
+print("-=-" * 19)
+
+USERinputOne = input("(1).Calcular (2).Sobre (3).Sair")
